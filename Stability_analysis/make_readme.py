57bb64931a3c63859a024c152cf5a188eebbc9d3
Doug Latornell <djl@douglatornell.ca>
2014-06-01 14:08:23 -0400

    Fix typo in repo URL.

--- Stability_analysis/make_readme.py
+++ Stability_analysis/make_readme.py
@@ -23,7 +23,7 @@ import re
 
 
 nbviewer = 'http://nbviewer.ipython.org/urls'
-repo = 'bitbucket.org/salishsea/analysi/raw/tip'
+repo = 'bitbucket.org/salishsea/analysis/raw/tip'
 repo_dir = 'Stability_analysis'
 url = os.path.join(nbviewer, repo, repo_dir)
 title_pattern = re.compile('#{1,6} ?')
