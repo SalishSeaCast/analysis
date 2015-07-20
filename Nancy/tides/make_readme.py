a6ef2667e1b061b03f30dcb37691e08eca4f58b3
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2015-07-20 14:42:17 -0700

    Fixed readme

--- Nancy/tides/make_readme.py
+++ Nancy/tides/make_readme.py
@@ -24,7 +24,7 @@ import re
 
 nbviewer = 'http://nbviewer.ipython.org/urls'
 repo = 'bitbucket.org/salishsea/analysis/raw/tip'
-repo_dir = 'Nancy'
+repo_dir = 'Nancy/tides'
 url = os.path.join(nbviewer, repo, repo_dir)
 title_pattern = re.compile('#{1,6} ?')
 readme = """The IPython Notebooks in this directory are made by Nancy for
