aed812ca636704980c0072fbd0c0b4fb857fdc43
Nancy Soontiens <nsoontie@eos.ubc.ca>
2015-07-31 16:38:49 -0700

    Looked again at HYCOM stuff

--- Nancy/nowcast/grab_HYCOM.py
+++ Nancy/nowcast/grab_HYCOM.py
@@ -3,7 +3,8 @@
 from __future__ import division
 
 import numpy as np
-
+import pandas as pd
+import netCDF4 as nc
 import datetime
 import os
 import urllib2
@@ -54,6 +55,94 @@ def read_url(date, i, j):
     text_file.close()
 
     return filename
+
+
+def parse_hycom_text(filename):
+    """Parses the text in a output file from the hycom model.
+
+    :arg filename: file where the hycom model data is stored
+    :type filename: string
+
+    :returns: data, lon, lat
+    data is a data frame with ssh and time columns
+    lon is the longitude of the hycom model point
+    lat is the latitude of the hycom grid point
+    """
+    ssh_read = False
+    time_read = False
+    lat_read = False
+    lon_read = False
+    # initialize variables
+    sshs = []
+    times = []
+    lat = 0
+    lon = 0
+
+    # variable to define number of lines to skip
+    skip_lines = 0
+    with open(filename) as f:
+        # loop through each line
+        for line in f:
+            # check if we should skip a line
+            if skip_lines > 0:
+                skip_lines = skip_lines - 1
+                continue
+
+            # read the line
+            words = line.split()
+            if words:  # there is data in the line, do stuff with it
+                # if we should read a variable, read it
+                # read ssh
+                if ssh_read:
+                    if words[0] == 'time,':  # check we are in the ssh part
+                        time_read = True
+                        ssh_read = False
+                    else:
+                        sshs.append(float(words[1]))  # append the ssh to list
+                        skip_lines = 2
+                # read time
+                elif time_read:
+                    if words[0] == 'lev,':  # check we are still in time part
+                        time_read = False
+                    else:
+                        times = words
+                # read lat
+                elif lat_read:
+                    lat = float(words[0])
+                    lat_read = False
+                # read lon
+                elif lon_read:
+                    lon = float(words[0]) - 360
+                    # subtract 360 for conversion to model coordinates
+
+                # if we aren't reading a variable, check that we can
+                # determine which variable should be read next
+                if words[0] == 'ssh,':
+                    ssh_read = True
+                elif words[0] == 'time,':
+                    time_read = True
+                    ssh_read = False
+                elif words[0] == 'lat,':
+                    lat_read = True
+                    time_read = False
+                elif words[0] == 'lon,':
+                    lon_read = True
+                    lat_read = False
+    # finished reading the file
+
+    # convert times to datetimes
+    time_units = 'days since 1-1-1 00:00:0.0'
+    for i, t in enumerate(times):
+        t = float(t[:-1])
+        times[i] = nc.num2date(t, time_units)
+    # remove first ssh/times element because it is not real data
+    sshs = sshs[1:]
+    times = times[1:]
+
+    # add the data to a data frame
+    data = pd.DataFrame({'ssh': sshs, 'time': times})
+
+    return data, lon, lat
 
 if __name__ == '__main__':
     main()
