d76600a38387113d8730a5d8d5b74774d1b847ba
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2015-10-30 10:23:18 -0700

    Worked scripts as functions

--- Nancy/currents/t_tide_analysis/area_surface_tides.m
+++ Nancy/currents/t_tide_analysis/area_surface_tides.m
@@ -8,6 +8,8 @@ depav = 0; %depth average
 dlevel = 1; % surface
 trun = 0; %truncate water column
 d1 = 0; d2 = 0; %depth range
+%Define initial time index - default is one
+t0=1
 
 % load data
 [u, v, depth, time, lons, lats] = load_netcdf(filename);
@@ -15,7 +17,7 @@ d1 = 0; d2 = 0; %depth range
 %prepare time
 ref_time = [2014, 09, 10];
 mtimes = time_to_mtime(time, ref_time); 
-start = mtimes(1);
+start = mtimes(t0);
 
 %initialize strucuure for saving data array
 area = squeeze(size(u(:,:,1,1)));
@@ -28,8 +30,8 @@ datastruc = struct('lats',lats(2:end,2:end), 'lons', lons(2:end,2:end));
 tide_count=0;
 for i=1:Nx-1
     for j=1:Ny-1
-        urot = u(i:i+1,j:j+1,:,:);
-        vrot = v(i:i+1,j:j+1,:,:);
+        urot = u(i:i+1,j:j+1,:,t0:end);
+        vrot = v(i:i+1,j:j+1,:,t0:end);
         % prepare velocities for tidal analysis
         % That is, mask, unstagger and rotate
         [urot, vrot] = prepare_velocities(urot, vrot);
