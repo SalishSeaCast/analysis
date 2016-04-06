828da72975d480d4befe7a71400d282e2738dcbc
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2016-04-06 16:14:36 -0700

    Dealt with new wat to calculate baroclinic pressure

--- Nancy/energy_flux/energy_flux.py
+++ Nancy/energy_flux/energy_flux.py
@@ -3,10 +3,6 @@
 # NKS 2015
 
 import numpy as np
-import scipy.io as sio
-import netCDF4 as nc
-
-import os
 
 g = 9.81  # acceleration due to gravity m/s^2
 rho0 = 1035  # reference density (kg/m^3)
@@ -14,7 +10,8 @@ rho0 = 1035  # reference density (kg/m^3)
 
 def barotropic_pressure(pstruc, const):
     """Calculates the amplitude and phase of the barotropic
-     pressure signal for a constituent"""
+    pressure signal for a constituent.
+    pstruc contains analyzed sea suface height."""
 
     var = pstruc[const]
 
@@ -30,6 +27,9 @@ def barotropic_pressure(pstruc, const):
 
 
 def ellipse_constituent(cstruc, const):
+    """Return major, minor, phase and inclination for const given currents
+    analyzed in cstruc"""
+
     var = cstruc[const]
 
     major = var[0, 0]['major'][0, 0][:]
@@ -53,7 +53,9 @@ def cph2rps(freq):
 
 
 def baroclinic_pressure(pstruc, const):
-
+    """Return baroclinic pressure amplitude and phase for a constituent.
+    First way of calculating baroclinic pressure
+    dp/dt = -int(rho0*w*N^2dz)rho*g"""
     var = pstruc[const]
     # t_tide records frequency in cylces/hour
     # convert to
@@ -71,6 +73,20 @@ def baroclinic_pressure(pstruc, const):
     return p_amp, p_phase
 
 
+def baroclinic_pressure_try2(pstruc, const):
+    """Return baroclinic pressure amplitude and phase for a constituent.
+    Second way of calculating baroclinic pressure, dp/dz = -rho*g"""
+    var = pstruc[const]
+
+    amp = var[0, 0]['ampl'][0, 0][:]
+    amp = np.ma.masked_invalid(amp)
+
+    phase = var[0, 0]['phas'][0, 0][:]
+    phase = np.ma.masked_invalid(phase)
+
+    return amp, phase
+
+
 def flux_vectors(major, minor, phase, incl, p_amp, p_phase):
     """ Calculate flux vectors in north-south coordinates"""
     Fmaj = 0.5*major*p_amp*np.cos(np.deg2rad(phase) - np.deg2rad(p_phase))
@@ -86,12 +102,20 @@ def barotropic_flux(pstruc, cstruc, const):
     major, minor, phase, incl = ellipse_constituent(cstruc, const)
     p_amp, p_phase = barotropic_pressure(pstruc, const)
 
-    Fx, Fy  = flux_vectors(major, minor, phase, incl, p_amp, p_phase)
+    Fx, Fy = flux_vectors(major, minor, phase, incl, p_amp, p_phase)
 
     return Fx, Fy
 
 
 def baroclinic_flux(pstruc, cstruc, const):
+    """Caclualte baroclinic flux with first method of analyzing baroclinic
+    pressure. p = - int(rho0*w*N^2dz)
+
+    pstruc is the structure the contains the analyzed baroclinic dp/dt
+    cstruc is the structure the contains the analuzed baroclinic currents
+    const is the constituent to calculate the flux for
+    """
+
     major, minor, phase, incl = ellipse_constituent(cstruc, const)
     p_amp, p_phase = baroclinic_pressure(pstruc, const)
 
@@ -100,14 +124,31 @@ def baroclinic_flux(pstruc, cstruc, const):
     return Fx, Fy
 
 
+def baroclinic_flux_try2(pstruc, cstruc, const):
+    """Caclualte baroclinic flux with second method of analysig baroclinic
+    pressure. dp/dz = -rho*g
+
+    pstruc is the structure the contains the analyzed baroclinic p
+    cstruc is the structure the contains the analuzed baroclinic currents
+    const is the constituent to calculate the flux for
+    """
+
+    major, minor, phase, incl = ellipse_constituent(cstruc, const)
+    p_amp, p_phase = baroclinic_pressure_try2(pstruc, const)
+
+    Fx, Fy = flux_vectors(major, minor, phase, incl, p_amp, p_phase)
+
+    return Fx, Fy
+
+
 def find_starting_index(NEMO_lons, NEMO_lats, lon, lat):
     """Find grid index of lon, lat"""
-    j,i = np.where(np.logical_and(NEMO_lons==lon, NEMO_lats==lat))
+    j, i = np.where(np.logical_and(NEMO_lons == lon, NEMO_lats == lat))
 
-    return j,i
+    return j, i
 
 
-def water_depth(tmask,e3t):
+def water_depth(tmask, e3t):
     """Cacluate the total water depth"""
     water_depth = np.sum(e3t*tmask, axis=0)
     water_depth = np.ma.masked_values(water_depth, 0)
