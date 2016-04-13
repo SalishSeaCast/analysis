0b7c5d2e9a003a632dc554c0c1a7cab51c6ed06d
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2016-04-12 17:44:54 -0700

    redid energy flux pltos

--- Nancy/currents/VENUS_time_series.py
+++ Nancy/currents/VENUS_time_series.py
@@ -5,16 +5,16 @@ import datetime
 import netCDF4 as nc
 
 from salishsea_tools import (tidetools, ellipse)
-from salishsea_tools.nowcast import (research_VENUS)
+from nowcast import (research_VENUS)
 
 import baroclinic
 
 NodalCorr = tidetools.CorrTides
 
 
-to = datetime.datetime(2014, 11, 26)
-tf = datetime.datetime(2015, 4, 26)
-path = '/data/dlatorne/MEOPAR/SalishSea/nowcast/'
+to = datetime.datetime(2015, 10, 1)
+tf = datetime.datetime(2016, 3, 30)
+path = '/results/SalishSea/nowcast/'
 grid = nc.Dataset('/data/nsoontie/MEOPAR/NEMO-forcing/grid/bathy_meter_SalishSea2.nc')
 lon_grid = grid.variables['nav_lon']
 lat_grid = grid.variables['nav_lat']
@@ -30,4 +30,4 @@ for site in SITES:
     lats = lat_grid[j-1:j+1, i-1:i+1]
 
     baroclinic.save_netcdf(times, us, vs, depths, site, lons, lats, to, tf)
-    print 'Saved {}'.format(site)
+    print ('Saved {}'.format(site))
